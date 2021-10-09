1d8e0213703c11651b8e3e46e94ec8490f69225e
David Wilkie <david.wilkie@gmail.com>
2021-10-09 16:10:45 +0100

    feat: Read supplemental tags from data folder

--- src/util/transformMapMyRunRow.js
+++ src/util/transformMapMyRunRow.js
@@ -1,5 +1,6 @@
 import { COLS } from '../constants/mapMyRunColumn';
 import { dateFormatter } from './mapMyRunDateFormatter';
+import tagData from '../../data/tags/tags';
 
 const MOUNTAIN_BIKING = 'Mountain Biking';
 
@@ -7,15 +8,7 @@ const sportTransform = {
     'Walk': 'Walking',
     'Bike Ride': 'Cycling (Sport)',
     'Run': 'Running',
-
-    // 'MOUNTAIN_BIKING': 'Mountain Biking',
-    // 'SPINNING': 'Turbo Trainer',
-    // 'CYCLING_TRANSPORTATION': 'Cycling (Transport)',
-    // 'RUNNING': 'Running',
-    // 'SNOWBOARDING': 'Snowboarding',
-    // 'CLIMBING': 'Climbing',
-    // 'CROSSFIT': 'Crossfit',
-    // 'ICE_SKATING': 'Ice Skating'  
+    'Machine Workout': 'Rowing Machine',
 };
 
 export const transformMapMyRunRow = (row) => {
@@ -35,54 +28,19 @@ export const transformMapMyRunRow = (row) => {
         sport = MOUNTAIN_BIKING;
     } 
 
-    // TODO: More generic way to handle tags for parts which aren't tagged in Map My Run
-    if (tags.includes('CORTEX')) {
-        if (start_time < (new Date(2021, 2, 22).getTime())) {
-            tags.push('TRAILBOSS3');
+    Object.keys(tagData).forEach(parentTag => {
+        if (tags.includes(parentTag)) {
+            const tagDataRows = tagData[parentTag];
+            tagDataRows.forEach(tagDataRow => {
+                if (
+                    (!tagDataRow.startDate || (tagDataRow.startDate && start_time >= new Date(tagDataRow.startDate))) &&
+                    (!tagDataRow.endDate || (tagDataRow.endDate && start_time < new Date(tagDataRow.endDate)))
+                ) {
+                    tags.push(tagDataRow.tag);
+                }
+            });
         }
-
-        // 21st March 2021 new rear tyre
-        if (start_time > (new Date(2021, 2, 21).getTime())) {
-            tags.push('MAXXISMINION');
-        }
-
-        // 21st March 2021 new tube for rear tyre, 19th July 2021 puncture in tube, revert to tubless
-        if (start_time > (new Date(2021, 2, 21).getTime()) && start_time < (new Date(2021, 6, 19).getTime())) {
-            tags.push('TUBE30');
-        }
-
-        // 27th July 2021 New pads for cortex - track stock brakes since new before then        
-        if (start_time < (new Date(2021, 6, 27).getTime())) {
-            tags.push('FRONTBRAKE12');
-            tags.push('REARBRAKE12');
-        }
-
-        // 27th July 2021 New pads for cortex
-        if (start_time >= (new Date(2021, 6, 26).getTime())) {
-            tags.push('FRONTBRAKE12A');
-            tags.push('REARBRAKE12A');
-        }
-    }
-
-    if (tags.includes('FRONTIER')) {
-        if (start_time > (new Date(2021, 2, 27).getTime())) {
-            tags.push('XKING1');
-            tags.push('REARMECH6C');
-            tags.push('MOUNTAINKING6');
-        }
-
-        // 02/08/2021 Moved cassette/chain from ariel onto frontier
-        if (start_time > (new Date(2021, 7, 2).getTime())) {
-            tags.push('CASETTE16A');
-            tags.push('CHAIN16A');
-        }
-
-        // 08/10/2021 Moved caliper from ariel/rear rotor onto frontier
-        if (start_time > (new Date(2021, 9, 8).getTime())) {
-            tags.push('REARBRAKE1S');
-            tags.push('ROTOR5A');
-        }        
-    }
+    })
 
     return {
         sport,
